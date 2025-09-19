d787d65ae693a8135e85bad059c26a8c916b2b87
ck236385 <ck236385@gmail.com>
2025-09-19 10:49:20 +0000

    Done

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -1,9 +1,9 @@
-import {create} from 'zustand';
+import { create } from 'zustand/traditional';
 
 //this is same things like:- using useState for defining state and setterFunction;
 const useConversation = create((set) => ({
     selectedConversation:null,
-    setSelectedConversation:(selectedConversation) => set(({selectedConversation})),
+    setSelectedConversation:(selectedConversation) => set({ selectedConversation }),
     messages:[],
     setMessages: (messages) => set({ messages }),
 
