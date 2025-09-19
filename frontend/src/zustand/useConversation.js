605f186a20967748c17fa8790dd2f7903ca7caf9
ck236385 <ck236385@gmail.com>
2025-09-19 10:54:31 +0000

    Done

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -1,12 +1,10 @@
-import { create } from 'zustand/traditional';
+import { createWithEqualityFn } from 'zustand/traditional';
 
-//this is same things like:- using useState for defining state and setterFunction;
-const useConversation = create((set) => ({
-    selectedConversation:null,
-    setSelectedConversation:(selectedConversation) => set({ selectedConversation }),
-    messages:[],
+const useConversation = createWithEqualityFn((set) => ({
+    selectedConversation: null,
+    setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
+    messages: [],
     setMessages: (messages) => set({ messages }),
-
 }));
 
 export default useConversation;
